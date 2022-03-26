ac578b466beb135120b65c4a4b086a4a8790d1be
ahmed91abbas <ahmed91abbas@hotmail.com>
2022-03-26 23:52:05 +0100

    implement booking confirmation step

--- passport_time_booker.py
+++ passport_time_booker.py
@@ -11,20 +11,18 @@ from collections import defaultdict
 
 
 class PassportTimeBooker:
-    def __init__(self, url, config_path):
+    def __init__(self, url, config_file_path, confirmations_file_path):
         self.url = url
-        self.config = self.get_config(config_path)
+        self.confirmations_file_path = confirmations_file_path
+        self.config = self.get_config(config_file_path)
 
-
-    def get_config(self, config_path):
-        with open(config_path, 'r') as f:
+    def get_config(self, config_file_path):
+        with open(config_file_path, 'r') as f:
             return json.loads(f.read())
-
 
     def book_passport_time(self):
         self.init_driver()
         self.navigate_to_times_page()
-
         accepted_date = None
         while not accepted_date:
             page_source = self.get_available_times_source()
@@ -33,10 +31,8 @@ class PassportTimeBooker:
             if accepted_date:
                 break
             sleep(10)
-
-        self.driver.find_element(by=By.CSS_SELECTOR, value=f"[aria-label=\'{accepted_date}\']").click()
-        self.driver.find_element(by=By.NAME, value="Next").click()
-        input()
+        self.confirm_booking(accepted_date)
+        self.write_confirmation_to_file()
 
     def init_driver(self):
         options = Options()
@@ -46,13 +42,17 @@ class PassportTimeBooker:
 
     def navigate_to_times_page(self):
         self.driver.get(self.url)
+        # page: Välkommen till tidsbokningen i Skåne län
         self.driver.find_element(by=By.NAME, value="StartNextButton").click()
+        # page: Behandling av personuppgifter
         self.driver.find_element(by=By.NAME, value="AcceptInformationStorage").click()
         self.driver.find_element(by=By.NAME, value="Next").click()
+        # page: Bor du i Sverige?
         self.driver.find_element(by=By.ID, value="ServiceCategoryCustomers_0__ServiceCategoryId").click()
         self.driver.find_element(by=By.NAME, value="Next").click()
 
     def get_available_times_source(self):
+        # page: Välj tid
         self.driver.find_element(by=By.NAME, value="TimeSearchFirstAvailableButton").click()
         return self.driver.page_source
 
@@ -87,8 +87,46 @@ class PassportTimeBooker:
                         return date
         return None
 
+    def confirm_booking(self, date):
+        # page: Välj tid
+        self.driver.find_element(by=By.CSS_SELECTOR, value=f"[aria-label=\'{date}\']").click()
+        self.driver.find_element(by=By.NAME, value="Next").click()
+        # page: Uppgifter till bokningen
+        self.driver.find_element(by=By.ID, value="Customers_0__BookingFieldValues_0__Value").send_keys(self.config['first_name'])
+        self.driver.find_element(by=By.ID, value="Customers_0__BookingFieldValues_1__Value").send_keys(self.config['last_name'])
+        self.driver.find_element(by=By.ID, value="Customers_0__Services_0__IsSelected").click()
+        self.driver.find_element(by=By.NAME, value="Next").click()
+        # page: Viktig information
+        self.driver.find_element(by=By.NAME, value="Next").click()
+        # page: Kontaktuppgifter
+        self.driver.find_element(by=By.ID, value="EmailAddress").send_keys(self.config['email'])
+        self.driver.find_element(by=By.ID, value="ConfirmEmailAddress").send_keys(self.config['email'])
+        self.driver.find_element(by=By.ID, value="SelectedContacts_0__IsSelected").click()
+        self.driver.find_element(by=By.ID, value="SelectedContacts_2__IsSelected").click()
+        self.driver.find_element(by=By.NAME, value="Next").click()
+        # page: Bekräfta bokning
+        self.driver.find_element(by=By.NAME, value="Next").click()
+
+    def write_confirmation_to_file(self):
+        page_source = self.driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+        labels = soup.find_all('label', {'class':'control-label'})
+        values = soup.find_all('b', {'class':'control-freetext'}) + soup.find_all('span', {'class':'control-freetext'})
+        pairs = list(zip(labels, values))
+        details = [f'{label.string} {value.string}' for label, value in pairs if value.string is not None]
+        details.append(f'Namn: {self.config["first_name"]} {self.config["last_name"]}')
+        details.append(f'Email: {self.config["email"]}')
+        details = '\n'.join(details)
+        with open(self.confirmations_file_path, 'a+', encoding='utf-8') as f:
+            seperator = '*'*60
+            f.write(details)
+            f.write(f'\n\n{seperator}\n\n')
+
 
 if __name__ == '__main__':
     url = 'https://bokapass.nemoq.se/Booking/Booking/Index/skane'
-    booker = PassportTimeBooker(url, 'config.json')
+    config_file_path = 'config.json'
+    confirmations_file_path = 'confirmations.txt'
+    booker = PassportTimeBooker(url, config_file_path, confirmations_file_path)
     booker.book_passport_time()
+    input()
